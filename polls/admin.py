a10ae91c6df0beb1fc202719bb0e0d38314b9e92
Pablo.Fuentes <pablo.fuentes@intelygez.com>
2018-10-23 20:50:46 +0200

    update git files

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import Question, Choice
 # Register your models here.
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
